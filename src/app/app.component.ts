437589bf5926bfe7ff16217da96fffcf5a7f13e6
Priyanshu7129 <priyanshu7129@gmail.com>
2025-03-24 23:03:57 +0530

    Added One-Way Data Binding using Interpolation in UC1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector:'app-root',
+  templateUrl:'./app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Hello from BridgeLabz';
+export class AppComponent{
+  title = 'HelloWorld';
+  ngOnInit(): void {
+    this.title = "Hello from bridgelabz.";
+  }
 }
